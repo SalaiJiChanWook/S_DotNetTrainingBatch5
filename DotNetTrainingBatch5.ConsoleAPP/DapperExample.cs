a7bb6d85eef2f525d4f1442547b668000c8c21e3
salai <uchiasadara6@gmail.com>
2024-09-16 22:40:44 +0630

    some read function

---
+++ DotNetTrainingBatch5.ConsoleAPP/DapperExample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTrainingBatch5.ConsoleAPP
+{
+
+    public class DapperExample
+    {
+        private readonly string _connectionString = "Data Source= UCHIASALAI\\SQLEXPRESS;Initial Catalog=DotNetTrainingBatch5;User ID=salai;Password=Vpjtqwv23@#";
+        public void Read()
+        {
+            //using(IDbConnection db = new SqlConnection(_connectionString))
+            //{
+            //    string query = "select * from tbl_blog where DeleteFlag = 0;";
+            //    var lst = db.Query(query).ToList();
+            //    foreach (var item in lst)
+            //    {
+            //        Console.WriteLine(item.BlogId);
+            //        Console.WriteLine(item.BlogTitle);
+            //        Console.WriteLine(item.BlogAuthor);
+            //        Console.WriteLine(item.BlogContent);
+            //    }
+            //}
+
+            //using (IDbConnection db = new SqlConnection(_connectionString))
+            //{
+            //    string query = "select * from tbl_blog where DeleteFlag = 0;";
+            //    var lst = db.Query<BlogDapperDataModel>(query).ToList();
+            //    foreach (var item in lst)
+            //    {
+            //        Console.WriteLine(item.BlogId);
+            //        Console.WriteLine(item.BlogTitle);
+            //        Console.WriteLine(item.BlogAuthor);
+            //        Console.WriteLine(item.BlogContent);
+            //    }
+            //}
+
+            // DTO =>  Data Transfer Object
+        }
+    }
+}
