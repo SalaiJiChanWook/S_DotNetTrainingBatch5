710f7c4b949d100e974fe7339ea7d2027a702676
salai <uchiasadara6@gmail.com>
2024-09-17 11:50:59 +0630

    added Create function

--- DotNetTrainingBatch5.ConsoleAPP/DapperExample.cs
+++ DotNetTrainingBatch5.ConsoleAPP/DapperExample.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
+using DotNetTrainingBatch5.ConsoleAPP.Models;
+using System.Security.Cryptography.X509Certificates;
 
 namespace DotNetTrainingBatch5.ConsoleAPP
 {
@@ -14,33 +17,46 @@ namespace DotNetTrainingBatch5.ConsoleAPP
         private readonly string _connectionString = "Data Source= UCHIASALAI\\SQLEXPRESS;Initial Catalog=DotNetTrainingBatch5;User ID=salai;Password=Vpjtqwv23@#";
         public void Read()
         {
-            //using(IDbConnection db = new SqlConnection(_connectionString))
-            //{
-            //    string query = "select * from tbl_blog where DeleteFlag = 0;";
-            //    var lst = db.Query(query).ToList();
-            //    foreach (var item in lst)
-            //    {
-            //        Console.WriteLine(item.BlogId);
-            //        Console.WriteLine(item.BlogTitle);
-            //        Console.WriteLine(item.BlogAuthor);
-            //        Console.WriteLine(item.BlogContent);
-            //    }
-            //}
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                string query = "select * from Tbl_blog where DeleteFlag = 0;";
+                var lst = db.Query<BlogDataModel>(query).ToList();
+                foreach (var item in lst)
+                {
+                    Console.WriteLine(item.BlogId);
+                    Console.WriteLine(item.BlogTitle);
+                    Console.WriteLine(item.BlogAuthor);
+                    Console.WriteLine(item.BlogContent);
+                }
 
-            //using (IDbConnection db = new SqlConnection(_connectionString))
-            //{
-            //    string query = "select * from tbl_blog where DeleteFlag = 0;";
-            //    var lst = db.Query<BlogDapperDataModel>(query).ToList();
-            //    foreach (var item in lst)
-            //    {
-            //        Console.WriteLine(item.BlogId);
-            //        Console.WriteLine(item.BlogTitle);
-            //        Console.WriteLine(item.BlogAuthor);
-            //        Console.WriteLine(item.BlogContent);
-            //    }
-            //}
+            }
 
-            // DTO =>  Data Transfer Object
+            // DTO => Data Transfer Object
+        }
+        public void Create(string title, string author, string content)
+        {
+            string query = $@"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent]
+           ,[DeleteFlag])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent
+           ,0)";
+
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                int result = db.Execute(query, new BlogDataModel
+                {
+                    BlogTitle = title,
+                    BlogAuthor = author,
+                    BlogContent = content });
+                Console.WriteLine(result == 1 ? "Data Saving is Successful." : "Data Saving was Failed.");
+
+            }
+
         }
     }
 }
